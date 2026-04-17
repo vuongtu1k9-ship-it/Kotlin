5f69e732a891d81959f8bb1ee9eb3014c6164496
vuongtu1k9-ship-it <vuongtu1k9@gmail.com>
2026-04-17 06:05:21 +0000

    feat: Tic-Tac-Toe app only - remove unrelated files

--- simple-counter-app/app/src/main/java/com/example/counter/MainApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.counter
-
-import android.app.Application
-
-class MainApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
--- start_websocket.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script khởi động WebSocket Server (Kotlin)
-
-cd /root/Kotlin/
-kotor -cp "$(kotlinc WebSocketServer.kt -cp "app/build/libs/*" -d . -include-runtime)" com.xiangqi.WebSocketServerKt
