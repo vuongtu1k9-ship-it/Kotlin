abb3342435258ffb987ae48b4bdb9d118939fd7c
vuongtu1k9 <vuongtu1k9@gmail.com>
2026-04-17 02:23:14 +0000

    Fix: Downgrade Material3 to 1.2.0 (final)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui:1.6.3")
-    implementation("androidx.compose.material3:material3:1.6.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.3")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.3")
 }
